6c3fc3f5fc49609019b6c8237a7a2951a9d9cd44
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-03-14 14:39:45 +0200

    Add countries array

--- src/components/countries/CountriesList.jsx
+++ src/components/countries/CountriesList.jsx
@@ -1,9 +1,22 @@
 import { NavLink } from 'react-router-dom';
 
+import countriesData from '../../countriesArray';
+
 const CountriesList = () => (
   <div>
-    <h2>Country list</h2>
-    <NavLink to="/stats">Country</NavLink>
+    <header>
+      <h2>Country list</h2>
+    </header>
+
+    <ul className="row">
+      {countriesData.map((data) => (
+        <li key={data.country} className="col-3 p-1 mb-2">
+          <NavLink style={{ width: '100%' }} className=" p-2 border" to="/stats">
+            <h3>{data.country}</h3>
+          </NavLink>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
